48ad557e4953d8a385d50ee2ca8797c58e959fc7
chaiwen <cc3636@columbia.edu>
2015-04-26 17:54:39 -0400

    if statements

--- translator/LeverToJavaListener.java
+++ translator/LeverToJavaListener.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 
 import org.antlr.v4.runtime.tree.*;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
 
 //don't need to override every enter/exit method
 public class LeverToJavaListener extends LeverBaseListener {
@@ -20,8 +21,10 @@ public class LeverToJavaListener extends LeverBaseListener {
 	private File targetFile;
 	private BufferedWriter bw;
 
-	public LeverToJavaListener(String _fileName) {
-		//this.parser = parser;
+	private int indents;
+
+	public LeverToJavaListener(LeverParser parser, String _fileName) {
+		this.parser = parser;
 		//String fileName = _fileName.split(".")[0];
 		Path p = Paths.get(_fileName);
 		fileName = p.getFileName().toString();
@@ -46,6 +49,26 @@ public class LeverToJavaListener extends LeverBaseListener {
 			System.out.println("error creating target file..");
 			System.exit(1);
 		}
+
+		indents = 0;
+	}
+	private void openBraces() {
+		indents++;
+		printTarget("{\n");
+	}
+	private void closeBraces() {
+		indents--;
+		printTarget("\n");
+		printTabs();
+		printTarget("}\n");
+		
+	}
+	private void printTabs() {
+		int i = indents;
+		while (i > 0) {
+			printTarget("\t");
+			i--;
+		}
 	}
 	private void printTarget(String s) {
 		try {
@@ -58,19 +81,80 @@ public class LeverToJavaListener extends LeverBaseListener {
 
 	@Override
 	public void enterMainProgram(LeverParser.MainProgramContext ctx) {
-		printTarget("public class " + fileName + " {\n");
-		printTarget("\tpublic static void main(String[] args) {\n");
+		printTarget("public class " + fileName + " ");
+		openBraces();
+		printTarget("\tpublic static void main(String[] args) ");
 	}
 
 	@Override
 	public void exitMainProgram(LeverParser.MainProgramContext ctx) {
-		printTarget("\t}\n}");
+		closeBraces();
+
 		try {
 			bw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
+	}
+
+	@Override
+	public void enterBlock(LeverParser.BlockContext ctx) {
+		openBraces();
+	}
+	@Override
+	public void exitBlock(LeverParser.BlockContext ctx) {
+		closeBraces();
+	}
+
+	@Override
+	public void enterStatement(LeverParser.StatementContext ctx) {
+		printTarget("\n");
+		printTabs();
+		
+		//TokenStream tokens = parser.getTokenStream();
+		//String i = "blah";
+		if (ctx.IF() != null) {
+			//i = tokens.getText(ctx.IF());
+			//i = ctx.IF().toString();
+			//System.out.println("asdf " + i);
+		//	printTarget("if ");
+		}
+		
+		
+		
+	}
+
+//	@Override public void exitStatement(LeverParser.StatementContext ctx) { }
+	
+	@Override
+	public void enterForIn(LeverParser.ForInContext ctx) {
+		//TokenStream tokens = parser.getTokenStream();
+
+		TerminalNode begin = ctx.getToken(LeverLexer.NumberLiteral, 0);
+		TerminalNode end = ctx.getToken(LeverLexer.NumberLiteral, 1);
+
+		printTarget("for (int " + ctx.Identifier() + " = " + begin + "; ");
+		printTarget(ctx.Identifier() + " < " + end + "; " + ctx.Identifier() + "++) ");
+		
+	}
+	@Override
+	public void enterForEach(LeverParser.ForEachContext ctx) {
+		
+		
+	}
+
+
+
+	@Override
+	public void enterParExpression(LeverParser.ParExpressionContext ctx) {
+		
+		printTarget("(");
+	}
+	@Override
+	public void exitParExpression(LeverParser.ParExpressionContext ctx) {
+		printTarget(")");
+		
 	}
 
 	@Override
@@ -90,12 +174,21 @@ public class LeverToJavaListener extends LeverBaseListener {
 	}
 	@Override
 	public void exitStatementExpression(LeverParser.StatementExpressionContext ctx) {
-		printTarget(";\n");
+		printTarget(";");
 	}
 	
 	@Override
 	public void enterLiteral(LeverParser.LiteralContext ctx) {
 		//printTarget(ctx.StringLiteral().getText());
+		if (ctx.BooleanLiteral() != null) {
+			String lit = ctx.BooleanLiteral().toString();
+			if (lit.equals("yes") || lit.equals("true")) {
+				printTarget("true");
+			} else if (lit.equals("no") || lit.equals("false")) {
+				printTarget("false");
+			}
+			//System.out.println(ctx.BooleanLiteral().toString());
+		}
 	}
 	@Override
 	public void enterPrimary(LeverParser.PrimaryContext ctx) {
@@ -111,9 +204,17 @@ public class LeverToJavaListener extends LeverBaseListener {
 		switch(type) {
 			case LeverLexer.Identifier:
 				if (id.equals("output")) {
-					printTarget("LeverClasses.output");
+					printTarget("LeverAPI.output");
 				} else {
-					printTarget(id);
+
+					String tmp = node.getParent().getChild(0).toString();
+					if (tmp.equals("for")) {
+
+					} else {
+						printTarget(id);
+
+					}
+					//System.out.println(node.getParent().getChild(0) + ", " + id);
 				}
 				
 				break;
@@ -124,6 +225,26 @@ public class LeverToJavaListener extends LeverBaseListener {
 			case LeverLexer.StringLiteral:
 				printTarget(id);
 
+				break;
+
+			case LeverLexer.AND:
+				printTarget(" && ");
+				break;
+			case LeverLexer.OR:
+				printTarget(" || ");
+				break;
+			case LeverLexer.IF:
+				printTarget("if ");
+				break;
+			case LeverLexer.ELSE:
+				printTarget("else");
+				break;
+			case LeverLexer.WHILE:
+				printTarget("while ");
+				break;
+
+			case LeverLexer.ADD:
+				printTarget(" + ");
 				break;
 
 
