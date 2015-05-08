3f91f1ed3ebda93100632f11d000938be660d753
Zissis Konstas <zkonstas@gmail.com>
2015-05-08 15:56:01 -0400

    added var check

---
+++ translator/VariableCheckingListener.java
@@ -0,0 +1,20 @@
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import java.util.*;
+
+//don't need to override every enter/exit method
+public class LeverToJavaListener extends LeverBaseListener {
+	
+}
