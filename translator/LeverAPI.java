56282213ad14391a364fd0fcd036732c24c7f0d9
Roy Hermann <royhermann@dyn-160-39-203-111.dyn.columbia.edu>
2015-05-09 15:44:10 -0400

    updated files

--- translator/LeverAPI.java
+++ translator/LeverAPI.java
@@ -1,38 +1,33 @@
-
-
+import twitter4j.Status;
 import java.util.*;
 
 public class LeverAPI {
 
 
-	public static Result get(String arguments){
-		return QueryManager.getResultFromArguments(str);
-	}
+    public static Result get(String arguments) {
+        return QueryManager.getResultFromArguments(arguments);
+    }
 
-	pubic static void graph(String barType,String title, String[] xLabels, double[] dataPoints){
-		if(barType.equals("line"))
-			GraphManager.createLineChart(title, xLabels, dataPoints);
-		else
-			GraphManager.createBarChart(title,xLabels,dataPoints);
-	}
+    public static void graph(String barType, String title, String[] xLabels, double[] dataPoints) {
+        if (barType.equals("line"))
+            GraphManager.createLineChart(title, xLabels, dataPoints);
+        else
+            GraphManager.createBarChart(title, xLabels, dataPoints);
+    }
 
-	public static void output(Object obj) {
-		if (obj instanceof Status) {
-			//Object is a status
-			Status tweet = (Status) obj;
-			System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
-		} else if (obj instanceof User) {
-			User user = (User) obj;
-			System.out.println("@" + user.getScreenName());
-		}
-		else{
-			System.out.println(obj.toString());
-		}
+    public static void output(Object obj) {
+        if (obj instanceof Status) {
+            //Object is a status
+            Status tweet = (Status) obj;
+            System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+        } else {
+            System.out.println(obj.toString());
+        }
+    }
 
-	}
-	public static String input() {
-		Scanner in = new Scanner(System.in);
-		String s = in.nextLine();
-		return s;
-	}
+    public static String input() {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        return s;
+    }
 }
