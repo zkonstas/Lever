beed9bf1733a2d822e98e880423177f881c37758
chaiwen <cc3636@columbia.edu>
2015-05-09 23:12:47 -0400

    started demo 1 file

---
+++ leverSamples/LeverDemo1.java
@@ -0,0 +1,15 @@
+import sun.jvm.hotspot.utilities.Interval;
+import twitter4j.*;
+import twitter4j.User;
+
+import java.util.*;
+
+import LeverAPIPackage.*;
+
+public class LeverDemo1 {
+	public static void main(String[] args) {
+		result;
+
+	}
+
+}
